41f318ad35c3b0421823ffc2ebe5844e42022c9f
eemmiillyy <emily.morgan@code.berlin>
2021-09-27 11:57:02 +0200

    Add environment variable

--- src/fileTransformers/deployToVercelButton.ts
+++ src/fileTransformers/deployToVercelButton.ts
@@ -12,7 +12,7 @@ export const deployToVercelButton: FileTransformer = (params) => {
   ) {
     const encodedURI = encodeURI(parameters.repositoryOwner + '/' + parameters.repositoryHandle)
     content = content.concat(
-      `<br/><br/>[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/import?repository-url=https%3A%2F%2Fgithub.com%2F${encodedURI})
+      `<br/><br/>[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/import?repository-url=https%3A%2F%2Fgithub.com%2F${encodedURI}&env=DATABASE_URL&envDescription=Connection%20string%20for%20the%20database%20this%20deployment%20will%20talk%20to.%20If%20you're%20using%20the%20Data%20Proxy%2C%20then%20ude%20its%20connection%20string)
       `
     )
   }
