a085f7fe10d4f1aec25e29ad031596b3add3928a
Jason Kuhrt <jasonkuhrt@me.com>
2021-10-13 17:19:19 -0400

    fix: generic find-replace for dataproxy dist tag

--- src/fileTransformers/engineType.ts
+++ src/fileTransformers/engineType.ts
@@ -5,18 +5,21 @@ export const engineType: FileTransformer = (params) => {
 
   let content = file.content
 
-  if (file.path === 'prisma/schema.prisma') {
-    if (parameters.engineType) {
-      content = content.replace(
-        `provider = "prisma-client-js"`,
-        `provider = "prisma-client-js" \n  engineType = "${parameters.engineType}"`
-      )
-    }
-  }
-  if (file.path === 'package.json') {
-    if (parameters.engineType && parameters.engineType === 'dataproxy') {
-      content = content.replace(`"@prisma/client": "3.1.1"`, `"@prisma/client": "dataproxy"`)
-    }
+  switch (file.path) {
+    case 'prisma/schema.prisma':
+      if (parameters.engineType) {
+        content = content.replace(
+          `provider = "prisma-client-js"`,
+          // eslint-disable-next-line
+          `provider = "prisma-client-js" \n  engineType = "${parameters.engineType}"`
+        )
+      }
+      break
+    case 'package.json':
+      if (parameters.engineType === 'dataproxy') {
+        content = content.replace(/"@prisma\/client": ".+"/, `"@prisma/client": "dataproxy"`)
+      }
+      break
   }
 
   return content
