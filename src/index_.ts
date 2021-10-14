e88988e09185b031fb41fc95e4c0ea974072b099
Siddhant <me@madebysid.com>
2021-10-14 14:31:29 +0200

    fix: env var uri encode

--- src/index_.ts
+++ src/index_.ts
@@ -8,8 +8,9 @@ export const Utils = {
   getVercelDeployButtonUrl(params: { repositoryOwner: string; repositoryHandle: string }) {
     const repository = encodeURIComponent(params.repositoryOwner + '/' + params.repositoryHandle)
     const envVars = ['DATABASE_URL', 'DATABASE_MIGRATE_URL'].join(',')
-    const envVarDescription =
+    const envVarDescription = encodeURIComponent(
       "Connection string for the database this deployment will talk to. If you're using the Prisma Data Proxy, then use its connection string."
+    )
 
     return `https://vercel.com/new/import?repository-url=https%3A%2F%2Fgithub.com%2F${repository}&env=${envVars}&envDescription=${envVarDescription}`
   },
