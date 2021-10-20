06670e01785219c7a2d33793e4e11027365f0fbb
Jason Kuhrt <jasonkuhrt@me.com>
2021-10-20 09:49:29 -0400

    tests: split template static and instance tests

---
+++ tests/templates-instances.spec.ts
@@ -0,0 +1,80 @@
+import { PrismaTemplates } from '~/src'
+import { PrismaDatasourceProviderName } from '~/src/data/prisma'
+
+describe('with custom datasourceProvider', () => {
+  Object.values(PrismaDatasourceProviderName).forEach((datasourceProvider) => {
+    Object.values(PrismaTemplates.Templates).forEach((Template) => {
+      it(`${Template.metadata.displayName} X ${datasourceProvider}`, () => {
+        const template = new Template({
+          datasourceProvider,
+        })
+        expect(template).toMatchSnapshot()
+      })
+    })
+  })
+})
+
+describe('with custom repository owner and repository handle', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      const template = new Template({
+        datasourceProvider: 'mysql',
+        repositoryOwner: 'prisma',
+        repositoryHandle: 'templates-node',
+      })
+      expect(template.files['README.md']).toMatchSnapshot()
+    })
+  })
+})
+
+describe('with custom engineType', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      const template = new Template({
+        datasourceProvider: 'mysql',
+        repositoryOwner: 'prisma',
+        repositoryHandle: 'templates-node',
+        engineType: 'library',
+      })
+      expect(template.files['prisma/schema.prisma']).toMatchSnapshot()
+    })
+  })
+})
+
+describe('with no custom engineType', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      const template = new Template({
+        datasourceProvider: 'mysql',
+      })
+      expect(template.files['prisma/schema.prisma']).toMatchSnapshot()
+    })
+  })
+})
+
+describe('with custom @prisma/client dependency not set to binary', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      const template = new Template({
+        datasourceProvider: 'mysql',
+        repositoryOwner: 'prisma',
+        repositoryHandle: 'templates-node',
+        engineType: 'library',
+      })
+      expect(template.files['package.json']).toMatchSnapshot()
+    })
+  })
+})
+
+describe('with custom @prisma/client dependency not set at all', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      const template = new Template({
+        datasourceProvider: 'mysql',
+        repositoryOwner: 'prisma',
+        repositoryHandle: 'templates-node',
+      })
+      expect(template.files['package.json']).toMatchSnapshot()
+    })
+  })
+})
---
+++ tests/templates-static-data.spec.ts
@@ -0,0 +1,30 @@
+import { PrismaTemplates } from '~/src'
+
+describe('files', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      expect(Template.files).toMatchSnapshot()
+    })
+  })
+})
+describe('artifacts', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      expect(Template.artifacts).toMatchSnapshot()
+    })
+  })
+})
+describe('metadata', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      expect(Template.metadata).toMatchSnapshot()
+    })
+  })
+})
+describe('handleMap', () => {
+  Object.values(PrismaTemplates.Templates).forEach((Template) => {
+    it(Template.metadata.displayName, () => {
+      expect(Template.handleMap).toMatchSnapshot()
+    })
+  })
+})
