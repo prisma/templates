47fa2a9f6dd874f12bc621715fa8df54581d1175
Jason Kuhrt <jasonkuhrt@me.com>
2022-06-06 19:30:38 +0000

    refactor

--- src/fileTransformers/datasourceProvider.ts
+++ src/fileTransformers/datasourceProvider.ts
@@ -1,5 +1,7 @@
 import { FileTransformer } from '../fileTransformer/fileTransformer'
+import { Reflector } from '../lib/Reflector'
 import { normalizeAutoincrement } from '../utils'
+import { pipe } from 'remeda'
 
 /**
  * When mongodb database provider is being used this will update the prisma client generator block to have the
@@ -8,23 +10,32 @@ import { normalizeAutoincrement } from '../utils'
  * @link https://www.prisma.io/docs/concepts/database-connectors/mongodb#example
  */
 export const datasourceProviderCockroachdb: FileTransformer = (params) => {
-  const { file, parameters, tools } = params
+  const { file, parameters } = params
 
   if (file.path === 'prisma/schema.prisma') {
-    if (parameters.datasourceProvider === 'mongodb') {
-      /**
-       * When mongodb database provider is being used this will update the prisma client generator block to have the
-       * needed preview feature flag enabled.
-       *
-       * @link https://www.prisma.io/docs/concepts/database-connectors/mongodb#example
-       */
-      return tools.prismaSchema.addPreviewFeatureFlag({
-        file: params.file,
-        PreviewFeatureFlag: 'mongoDb',
-      })
-    } else {
-      return normalizeAutoincrement(file.content, parameters.datasourceProvider)
-    }
+    return pipe(
+      file.content,
+      (prismaSchemaContent) =>
+        Reflector.Schema.setDatasourceProvider({
+          prismaSchemaContent,
+          value: parameters.datasourceProvider,
+        }),
+      (prismaSchemaContent) =>
+        parameters.datasourceProvider === 'mongodb'
+          ? /**
+             * When mongodb database provider is being used this will update the prisma client generator block to have the
+             * needed preview feature flag enabled.
+             *
+             * @link https://www.prisma.io/docs/concepts/database-connectors/mongodb#example
+             */
+            Reflector.Schema.addPreviewFeatureFlag({
+              prismaSchemaContent,
+              previewFlag: 'mongoDb',
+            })
+          : prismaSchemaContent,
+
+      (prismaSchemaContent) => normalizeAutoincrement(prismaSchemaContent, parameters.datasourceProvider)
+    )
   }
 
   return file.content
