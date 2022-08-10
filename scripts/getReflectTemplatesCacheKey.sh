c189a06fca9f8f0e0e7617762fe8020f91f0bb9a
eemmiillyy <emily.morgan@code.berlin>
2022-08-10 12:57:53 +0200

    check gh cache is being used

--- scripts/getReflectTemplatesCacheKey.sh
+++ scripts/getReflectTemplatesCacheKey.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-generatorHash=$(find ../generator -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
+generatorHash=$(find ./generator -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 
 # Base64 encode because GH cache action does not support at least commas.
 cacheKey=$(echo "{ \"generator\":\"$generatorHash\" }" | base64)
