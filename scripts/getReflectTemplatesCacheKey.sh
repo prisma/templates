fdb2e06f7477eb9fbd56789587f34b465fff6dfb
eemmiillyy <emily.morgan@code.berlin>
2022-08-10 16:42:44 +0200

    fix: rebuild when templates-raw changes

--- scripts/getReflectTemplatesCacheKey.sh
+++ scripts/getReflectTemplatesCacheKey.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+
+templatesHash=$(find ./templates-raw -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 generatorHash=$(find ./generator -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 
 # Base64 encode because GH cache action does not support at least commas.
-cacheKey=$(echo "{ \"generator\":\"$generatorHash\" }" | base64)
+cacheKey=$(echo "{ \"templates\":\"$templatesHash\", \"generator\":\"$generatorHash\" }" | base64)
 
 echo $cacheKey
 
