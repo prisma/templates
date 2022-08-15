281ed1add521817a448b7154fdf6dcf132202a93
Tatiana Inama <tatii.oxo@gmail.com>
2022-08-15 11:47:01 +0200

    deps: Upgrade reflector & prisma to 4.2.1 (#138)

--- scripts/getReflectTemplatesCacheKey.sh
+++ scripts/getReflectTemplatesCacheKey.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+dependenciesHash=$(find ./yarn.lock -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 templatesHash=$(find ./templates-raw -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 generatorHash=$(find ./generator -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
 
 # Base64 encode because GH cache action does not support at least commas.
-cacheKey=$(echo "{ \"templates\":\"$templatesHash\", \"generator\":\"$generatorHash\" }" | base64)
+cacheKey=$(echo "{ \"templates\":\"$templatesHash\", \"generator\":\"$generatorHash\", \"dependencies\": \"$dependenciesHash\" }" | base64)
 
 echo $cacheKey
-
