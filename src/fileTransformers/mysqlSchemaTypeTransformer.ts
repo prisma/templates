aa73ab90b065ebdf9ea4fda189b14a26ab0b0195
Jason Kuhrt <jasonkuhrt@me.com>
2022-08-15 10:16:55 +0200

    fix

--- src/fileTransformers/mysqlSchemaTypeTransformer.ts
+++ src/fileTransformers/mysqlSchemaTypeTransformer.ts
@@ -2,4 +2,6 @@ import { FileTransformer } from '~/src/fileTransformer/fileTransformer'
 import { mysqlSchemaTypeTransformUtil } from '~/src/utils'
 
 export const mysqlSchemaTypeTransformer: FileTransformer = (params) =>
-  mysqlSchemaTypeTransformUtil(params.parameters.datasourceProvider)(params.file.content)
+  params.file.path.includes('schema.prisma')
+    ? mysqlSchemaTypeTransformUtil(params.parameters.datasourceProvider)(params.file.content)
+    : params.file.content
